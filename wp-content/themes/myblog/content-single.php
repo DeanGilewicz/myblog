c66f6869e3c17dbeff584e471160004795227fe0
Dean Gilewicz <gilewiczd@gmail.com>
2017-02-26 17:12:06 -0500

    added breadcrumbs and basic content layout for single template

---
+++ wp-content/themes/myblog/content-single.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * An specfic template for displaying single post content
+ *
+ * @package WordPress
+ * @subpackage myblog
+ * @since myblog 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+	<div class="container_post">
+
+		<div class="post_feature_img">
+			<?php the_post_thumbnail(); ?>
+		</div>
+		
+		<header class="entry-header">
+
+			<div class="post_date">
+				<span><?php the_time('j'); ?></span>
+				<span><?php the_time('F'); ?></span>
+				<span><?php the_time('Y'); ?></span>
+			</div>
+
+			<div class="container_post_meta">
+				
+				<div class="post_title">
+					<?php
+					if ( is_single() ) :
+						the_title( '<h1 class="entry-title">', '</h1>' );
+					else :
+						the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+					endif;
+				?>
+				</div>
+
+				<div class="post_author">
+					<span>Written by <span><?php the_author(); ?></span></span>
+				</div>
+
+			</div>
+
+			<div class="post_type">
+				<i class="genericon genericon-pinned"></i>
+			</div>
+
+			<span class="post_cats">
+				<?php foreach ( get_the_category() as $cat ) : ?>
+					<?php 
+						$category_name  = $cat->cat_name;
+						$category_link  = get_category_link( $cat->cat_ID );
+					?>
+					<a href="<?php echo esc_url( $category_link ); ?>" title="<?php echo esc_attr( $category_name ); ?>"><?php echo esc_html( $category_name ); ?></a>
+				<?php endforeach; ?>
+			</span>
+
+			<span class="post_comments">
+				<a href="<?php echo get_comments_link( $post->ID ); ?>"><?php comments_number(); ?></a>
+			</span>
+			
+		</header><!-- .entry-header -->
+
+		
+
+		<div class="entry-content">
+
+			<?php
+				/* translators: %s: Name of current post */
+				// the_content( sprintf(
+				// 	__( 'Continue reading %s', 'myblog' ),
+				// 	the_title( '<span class="screen-reader-text">', '</span>', false )
+				// ) );
+				the_content();
+
+				// wp_link_pages( array(
+				// 	'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'myblog' ) . '</span>',
+				// 	'after'       => '</div>',
+				// 	'link_before' => '<span>',
+				// 	'link_after'  => '</span>',
+				// 	'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'myblog' ) . ' </span>%',
+				// 	'separator'   => '<span class="screen-reader-text">, </span>',
+				// ) );
+			?>
+
+		</div><!-- .entry-content -->
+
+		<footer class="entry-footer">
+
+			share on
+
+			<!-- <?php edit_post_link( __( 'Edit', 'myblog' ), '<span class="edit-link">', '</span>' ); ?> -->
+		
+		</footer><!-- .entry-footer -->
+
+	</div>
+
+</article><!-- #post-## -->
--- wp-content/themes/myblog/single.php
+++ wp-content/themes/myblog/single.php
@@ -11,37 +11,66 @@
 <?php get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main container_main_content" role="main">
 
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
+			<div class="dg_grid_container">
 
-			/*
-			 * Include the post format-specific template for the content. If you want to
-			 * use this in a child theme, then include a file called called content-___.php
-			 * (where ___ is the post format) and that will be used instead.
-			 */
-			get_template_part( 'content', get_post_format() );
+				<div class="dg_grid_row">
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+					<div class="dg_grid_col col_12">
 
-			// Previous/next post navigation.
-			the_post_navigation( array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'myblog' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Next post:', 'myblog' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'myblog' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Previous post:', 'myblog' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-			) );
+						<?php
+							if( function_exists('custom_breadcrumbs') ) {
+    							custom_breadcrumbs();
+							}
+						?>
 
-		// End the loop.
-		endwhile;
-		?>
+					</div>
+
+				</div>
+
+				<div class="dg_grid_row">
+
+					<div class="dg_grid_col col_12">
+
+						<?php
+						// Start the loop.
+						while ( have_posts() ) : the_post();
+
+							/*
+							 * Include the post format-specific template for the content. If you want to
+							 * use this in a child theme, then include a file called called content-___.php
+							 * (where ___ is the post format) and that will be used instead.
+							 */
+							// get_template_part( 'content', get_post_format() );
+
+							// override and get custom single content
+							get_template_part( 'content', 'single' );
+
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
+
+							// Previous/next post navigation.
+							the_post_navigation( array(
+								'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'myblog' ) . '</span> ' .
+									'<span class="screen-reader-text">' . __( 'Next post:', 'myblog' ) . '</span> ' .
+									'<span class="post-title">%title</span>',
+								'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'myblog' ) . '</span> ' .
+									'<span class="screen-reader-text">' . __( 'Previous post:', 'myblog' ) . '</span> ' .
+									'<span class="post-title">%title</span>',
+							) );
+
+						// End the loop.
+						endwhile;
+						?>
+
+					</div>
+
+				</div>
+
+			</div>
 
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
