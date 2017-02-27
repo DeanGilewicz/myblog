f621c3411ffe865baefb1855b14c86dc5b3db53b
Dean Gilewicz <gilewiczd@gmail.com>
2017-02-26 21:53:13 -0500

    updated comment styles

--- wp-content/themes/myblog/comments.php
+++ wp-content/themes/myblog/comments.php
@@ -20,39 +20,114 @@ if ( post_password_required() ) {
 }
 ?>
 
-<div id="comments" class="comments-area">
+<div class="dg_grid_container">
 
-	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'myblog' ),
-					number_format_i18n( get_comments_number() ), get_the_title() );
-			?>
-		</h2>
+	<div id="comments" class="comments-area">
 
-		<?php // myblog_comment_nav(); ?>
+		<?php if ( have_comments() ) : ?>
 
-		<ol class="comment-list">
-			<?php
-				wp_list_comments( array(
-					'style'       => 'ol',
-					'short_ping'  => true,
-					'avatar_size' => 56,
+		<div class="dg_grid_row">
+
+			<div class="dg_grid_col col_12">
+
+				<span class="post_comments">
+					<h6><?php comments_number(); ?></h6>
+				</span>		
+
+				<!--<h2 class="comments-title">
+					<?php
+						printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'myblog' ),
+							number_format_i18n( get_comments_number() ), get_the_title() );
+					?>
+				</h2>-->
+
+				<?php // myblog_comment_nav(); ?>
+
+			</div>
+
+		</div>
+
+		<div class="dg_grid_row">
+
+			<div class="dg_grid_col col_12">
+
+				<ul class="comment_list">
+					<?php
+						wp_list_comments( array(
+							'avatar_size' => 32
+						) );
+					?>
+				</ul><!-- .comment-list -->
+
+				<?php // myblog_comment_nav(); ?>
+
+			</div>
+
+		</div>
+
+		<?php endif; // have_comments() ?>
+
+		<div class="dg_grid_row">
+
+			<div class="dg_grid_col col_12">
+
+				<?php
+					// If comments are closed and there are comments, let's leave a little note, shall we?
+					if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+				?>
+					<p class="no-comments"><?php _e( 'Comments are closed.', 'myblog' ); ?></p>
+				<?php endif; ?>
+
+
+
+				<?php // comment_form(); ?>
+
+				<?php
+
+				$commenter = wp_get_current_commenter();
+				$req = get_option( 'require_name_email' );
+				$aria_req = ( $req ? " aria-required='true'" : '' );
+
+				$fields =  array(
+
+					'author' =>
+						'<p class="comment-form-author">
+							<label for="author">Name <span>*</span></label>
+							<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' />
+						</p>',
+
+					'email' =>
+						'<p class="comment-form-email">
+							<label for="email">Email <span>*</span></label>
+							<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' />
+						</p></div>'
+
+					// 'url' =>
+					// 	'<p class="comment-form-url"><label for="url">' . __( 'Website', 'domainreference' ) . '</label>' .
+					// 	'<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+					// 	'" size="30" /></p>'
+				);
+
+				// Comment form for users to leave a comment
+				comment_form( array(
+					'title_reply_before' => '<h2 id="reply-title" class="comment_reply_title">',
+					'title_reply'        => 'leave a reply',
+					'title_reply_after'  => '</h2>',
+					'comment_field' 	 => '<p class="comment_form_comment">
+												<label for="comment">Comment <span>*</span></label>
+		    									<textarea id="comment" name="comment" aria-required="true"></textarea>
+		    								</p>',
+		    		'comment_notes_after' => '<div class="respond-inputs">',
+		    		'class_submit' => 'button',
+		    		'label_submit' => 'Submit',
+		    		'fields' => apply_filters( 'comment_form_default_fields', $fields )
 				) );
 			?>
-		</ol><!-- .comment-list -->
 
-		<?php // myblog_comment_nav(); ?>
+			</div>
 
-	<?php endif; // have_comments() ?>
+		</div>
 
-	<?php
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'myblog' ); ?></p>
-	<?php endif; ?>
-
-	<?php comment_form(); ?>
+	</div>
 
 </div><!-- .comments-area -->
