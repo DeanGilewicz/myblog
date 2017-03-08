970393236705c0e2699c97909e32b4d017c967eb
Dean Gilewicz <gilewiczd@gmail.com>
2017-03-07 21:53:09 -0500

    created a partial for custom search form

--- wp-content/themes/myblog/index.php
+++ wp-content/themes/myblog/index.php
@@ -81,20 +81,7 @@
 									</nav>
 								<?php endif; ?>
 
-								<div class="container_search">
-
-									<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-										<!-- <label> -->
-											<!-- <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'myblog' ); ?></span> -->
-										<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'myblog' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'myblog' ); ?>" />
-										<!-- </label> -->
-										<button type="submit" class="search-submit button">
-											<!-- <span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'myblog' ); ?></span> -->
-											<i class="genericon genericon-search"></i>
-										</button>
-									</form>
-
-								</div>
+								<?php get_template_part( 'content', 'custom-search-form' ); ?>
 
 							<?php
 
